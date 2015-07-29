6a3e22f64241f20de6eeb310ac2433315059f76b
Soren Harner <sharner@permaling.com>
2015-07-28 20:59:39 -0700

    Updated to new leapmotion lib

--- package.js
+++ package.js
@@ -3,9 +3,10 @@ Package.describe({
 });
 
 Package.on_use(function(api){
-    api.add_files('js/leap.min.js','client');
+    api.add_files('js/leap.js','client');
     // api.add_files('js/leap-manager.js','client');
     // api.add_files('css/leap-manager.css','client');
-    // if(api.export)
-    //       api.export(['LeapManager'], ['client']);
+    if(api.export) {
+      api.export(['Leap'], ['client']);
+    }
 });
